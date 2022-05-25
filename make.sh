e2b499628febd6934187792bec484800e9b5bc11
Moffa Jérémy <moffajeremy@outlook.com>
2022-05-25 10:46:17 -0500

    Building for linux

--- make.sh
+++ make.sh
@@ -1,2 +1,4 @@
 #/bin/bash
 
+g++ -I"./third-party/openssl/include" -I"./third-party/secp256k1/include" -L"/usr/lib/x86_64-linux-gnu/" -pthread --std="c++17" "./WalletMiner/Source.cpp" -lsecp256k1 -lcrypto -lssl -o ./WMiner 
+
